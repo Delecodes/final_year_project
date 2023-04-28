8187c0c1546b8c1762a7082e013c0130e2ba1ae2
Ayodele <ayodeleaderibigbe2000@gmail.com>
2023-04-28 07:33:13 +0400

    Completed search functinality

--- components/Form.js
+++ components/Form.js
@@ -145,7 +145,6 @@ export default function Form() {
             <input
               type="tel"
               onChange={handleFormDataChange}
-              pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
               name="phoneNumber"
               id="floating_phone"
               class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
